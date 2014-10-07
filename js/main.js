fde3e8231e00e25fe750adc8d1317b325e9583d2
Mateus Velleda <mateusvelleda@gmail.com>
2014-10-07 01:19:35 -0300

    json added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-function cartController($scope) {
-	$scope.cart = [{
-		name: 'Arroz',
-		quantity: 2,
-		price: 2.38
-	}];
+var cartApp = angular.module('myCart', []);
+
+cartApp.config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        controller: 'CartCtrl',
+        templateUrl: 'index.html'
+    }).
+    when('/login', {
+        controller: 'LoginCtrl',
+        templateUrl: 'login.html'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+});
+
+cartApp.controller('CartCtrl', function ($scope) {
+	$scope.cart = [];
 	$scope.weekDay = function() {
 		return new Date().getDay();
 	};
+	$scope.surname = 'Vellar';
 	$scope.addProduct = function (productName, quantity, price) {
 		$scope.cart.push({
 			name: productName,
@@ -14,4 +28,22 @@ function cartController($scope) {
 			price: price
 		});
 	};
+});
+
+function CartCtrl($scope, $http) {
+	$scope.cart = [];
+	$scope.weekDay = function() {
+		return new Date().getDay();
+	};
+	$scope.surname = 'Vellar';
+	$scope.addProduct = function (productName, quantity, price) {
+		$scope.cart.push({
+			name: productName,
+			quantity: quantity,
+			price: price
+		});
+	};
+
+	$http.get("json/products.json")
+    .success(function(response) {$scope.cart = response;});
 }
